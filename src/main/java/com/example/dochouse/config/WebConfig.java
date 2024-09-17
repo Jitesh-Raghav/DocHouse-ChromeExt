00c1ad975a640ce1a200f2d5bcfcf2f6161074eb
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-09-17 23:12:35 +0530

    removed credentials

--- src/main/java/com/example/dochouse/config/WebConfig.java
+++ src/main/java/com/example/dochouse/config/WebConfig.java
@@ -11,10 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("chrome-extension://lldgdmkecjecbahnmjaapeeeecooajdp") // This allows all Chrome extensions
+                .allowedOrigins("chrome-extension://*") // This allows all Chrome extensions
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
 
